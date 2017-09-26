cd43acf3636b3e84e399a6d37de576758fcad957
Tyson Andre <tandre@themeetgroup.com>
2017-09-26 16:12:40 -0700

    Remove all namespaces from the test file.

--- invalid_read_test.php
+++ invalid_read_test.php
@@ -1,17 +1,5 @@
 <?php
 
-// This is a concatenated collection of multiple files, from Phockito and phpunit.
-// similar to https://github.com/ifwe/phockito
-// (This makes it easier to set up)
-// The original bug was found in a patched version of Phockito.
-// Currently, this emits invalid memory read errors if run in valgrind.
-
-
-
-namespace PHPUnit\Framework\Constraint {
-
-use SebastianBergmann\Exporter\Exporter;
-
 /**
  * Constraint that accepts any input value.
  */
@@ -19,45 +7,20 @@ class IsAnything
 {
     protected $exporter;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->exporter = new Exporter;
     }
 }
-}
 
-namespace SebastianBergmann\Exporter {
+class Exporter { }
 
-use SebastianBergmann\RecursionContext\Context;
-
-class Exporter
-{
-}
-}
-namespace{
-
-class __phockito_mockedclass_Mock extends mockedclass {
-
-}
+class __phockito_mockedclass_Mock {};
 
 class Phockito {
-	const MOCK_PREFIX = '__phockito_';
-
-    public static $_instanceid_counter = 0;
-
 	/* ** INTERNAL INTERFACES END ** */
 
-	/**
-	 * Given a class name as a string, return a new class name as a string which acts as a mock
-	 * of the passed class name. Probably not useful by itself until we start supporting static method stubbing
-	 *
-	 * @param string $class - The class to mock
-	 * @return object - The class that acts as a Phockito mock of the passed class
-	 */
 	static function mock(string $class) {
-		$mockClass = '__phockito_mockedclass_Mock';
-
-		return new $mockClass();
+		return new __phockito_mockedclass_Mock();
 	}
 
 	static function verify($mock, $times = 1) {
@@ -65,17 +28,9 @@ class Phockito {
 	}
 }
 
-/**
- * Marks all mocks for easy identification
- */
-interface Phockito_MockMarker {
-
-}
-
 class Phockito_VerifyBuilder {
 
 	function __call($called, $args) {
-        // The p
         printf("Verifying Phockito_Verify_Builder->%s\n", var_export($args, true));
         flush();
 
@@ -83,14 +38,6 @@ class Phockito_VerifyBuilder {
 	}
 }
 
-use PHPUnit\Framework\Assert;
-use PHPUnit\Framework\Constraint\IsAnything;
-
-/**
- * Returns a PHPUnit\Framework\Constraint\IsAnything matcher object.
- *
- * @return IsAnything
- */
 function anything()
 {
     return new IsAnything();
@@ -100,11 +47,9 @@ function anything()
 // This is the main part of the test case, below
 ////////////////////////////////////////////////////////////////////////////////
 
-class mockedclass {}
-
 class Test {
     public function outer() {
-        $mock = Phockito::mock('mockedclass');
+        $mock = new __phockito_mockedclass_Mock;
         Phockito::verify($mock, 1)->add(anything(), anything(), anything(), anything(), 1500, 3000, anything(), 'P', 3, anything(), anything(), 'U', anything(), anything(), 0);
     }
 }
@@ -114,4 +59,3 @@ function foo() {
     $t->outer();
 }
 foo();
-}
